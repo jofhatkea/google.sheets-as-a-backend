95ab8394401cdf4a9c9782f0e352931ec70d5ff6
Jonas Holbech <jh@jonasholbech.dk>
2017-08-30 12:17:51 +0200

    documentation

--- FetchGoogleJSON.js
+++ FetchGoogleJSON.js
@@ -1,11 +1,15 @@
 class FetchGoogleJSON {
-    constructor(url, callback){
+    constructor(url, callback, prettify=true){
         let re = /^https:/;
         if(!re.test(url)){
             url="https://spreadsheets.google.com/feeds/list/"+url+"/od6/public/values?alt=json";
         }
         fetch(url).then(e=>e.json()).then(d=>{
-            callback(new PrettifyGoogleJSON(d).get())
+            if(prettify){
+                callback(new PrettifyGoogleJSON(d).get());
+            } else {
+                callback(d);
+            }
         })
     }
 }
